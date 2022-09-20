7d088bd0ea617926ddc8346a8c1e10c30733c54e
lukee bajaa <57724155+lil-peen@users.noreply.github.com>
2022-09-19 21:34:09 -0400

    uhhhhh

--- src/data/tutorials/tutorialIndex.js
+++ src/data/tutorials/tutorialIndex.js
@@ -13,5 +13,15 @@ export default {
 			display: "HTML Basic Syntax",
 			prerequisites: ["html_hello_world"],
 		},
+
+		// incomplete
+		events_and_event_listeners: {
+			display: "Events & Event Listeners",
+			prerequisites: [],
+		},
+		setTimeout_setInterval: {
+			display: "setTimeout and setInterval",
+			prerequisites: [],
+		},
 	},
 };
